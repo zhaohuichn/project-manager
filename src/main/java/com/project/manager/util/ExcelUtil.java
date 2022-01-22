903dc68cb95c1ab2ec996f10a9499af89e3f211b
zhaohui <13651206093@163.com>
2022-01-22 19:02:17 +0800

    read

--- src/main/java/com/project/manager/util/ExcelUtil.java
+++ src/main/java/com/project/manager/util/ExcelUtil.java
@@ -1,7 +1,19 @@
 package com.project.manager.util;
 
-import java.io.InputStream;
-import java.net.URL;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.apache.catalina.mbeans.SparseUserDatabaseMBean;
+import org.apache.poi.ss.usermodel.CellType;
+import org.apache.poi.ss.usermodel.Header;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.Map;
 
 /**
  * ExcelUtil
@@ -12,11 +24,57 @@ public class ExcelUtil {
 
     /**
      * 从输入流中读取
+     *
      * @param inputStream
      * @return
      */
-    public byte[] read(InputStream inputStream) {
-        return null;
+    public static byte[] read(InputStream inputStream) {
+        try {
+            XSSFWorkbook excel = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = excel.getSheetAt(0);
+            XSSFRow sheetHeader = sheet.getRow(0);
+            // 列名
+            Map<Integer, String> cellNameMap = Maps.newHashMap();
+            for (int i = 0; i < sheetHeader.getLastCellNum(); i++) {
+                cellNameMap.put(i, getStringCellValue(sheetHeader.getCell(i)));
+            }
+
+            // 行列数据
+            Map<Integer, List<String>> rowData = Maps.newHashMap();
+            int rowNo = 0;
+            while ((rowNo++) < sheet.getLastRowNum()) {
+                XSSFRow row = sheet.getRow(rowNo);
+                int cellNo = 0;
+                List<String> cellValue = Lists.newLinkedList();
+                for (int index = 0; index < row.getLastCellNum(); index++) {
+                    cellValue.add(getStringCellValue(row.getCell(cellNo)));
+                    rowData.put(rowNo, cellValue);
+                }
+            }
+            return rowData.toString().getBytes(StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+
+
     }
+
+    private static String getStringCellValue(XSSFCell cell) {
+        if (null == cell) {
+            return "";
+        }
+        if (CellType.NUMERIC == cell.getCellType()) {
+            return String.valueOf(cell.getNumericCellValue());
+        } else {
+            return cell.getStringCellValue();
+        }
+
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        ExcelUtil.read(new FileInputStream("D:\\doc\\design\\Chailyn_20220122.xlsx"));
+    }
+
 
 }
