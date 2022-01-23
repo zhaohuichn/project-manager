b5fc659a1a1205b1621f2a63001ba02799ccf7c9
zhaohui <13651206093@163.com>
2022-01-23 10:40:54 +0800

    abs excel

--- src/main/java/com/project/manager/entity/po/source/excel/AbstractInputSource.java
+++ src/main/java/com/project/manager/entity/po/source/excel/AbstractInputSource.java
@@ -1,5 +1,8 @@
 package com.project.manager.entity.po.source.excel;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 /**
  * AbstractInputSource
  *
@@ -13,4 +16,18 @@ public abstract class AbstractInputSource implements InputSource {
         this.abstractPath = abstractPath;
     }
 
+    @Override
+    public String abstractPath() {
+        return this.abstractPath;
+    }
+
+    @Override
+    public URL url() {
+        try {
+            return new URL(this.abstractPath);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
---
+++ src/main/java/com/project/manager/entity/po/source/excel/StandInputExcel.java
@@ -0,0 +1,18 @@
+package com.project.manager.entity.po.source.excel;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * StandInputExcel
+ *
+ * @author ZHAOHUI
+ */
+public class StandInputExcel extends AbstractInputSource {
+
+    public StandInputExcel(String abstractPath) {
+        super(abstractPath);
+    }
+
+
+}
